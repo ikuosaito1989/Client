15f194e0c0be79f4eae112802269aaaf26ee45f3
ikuo saito <ikuosaito1989@gmail.com>
2021-11-26 22:38:31 +0900

    ASP.NET Core 3.1 から 5.0のバージョンアップ #40

--- SpotifyClient.cs
+++ SpotifyClient.cs
@@ -16,27 +16,27 @@ namespace webApi.Client
 
     public class SpotifyClient : ISpotifyClient
     {
-        private static SpotifyWebAPI _spotify;
+        private static SpotifyWebAPI s_spotify;
         public SpotifyClient(string clientId, string clientSecret)
         {
             var auth = new CredentialsAuth(clientId, clientSecret);
             var token = auth.GetToken().Result;
-            _spotify = new SpotifyWebAPI() { TokenType = token.TokenType, AccessToken = token.AccessToken };
+            s_spotify = new SpotifyWebAPI() { TokenType = token.TokenType, AccessToken = token.AccessToken };
         }
 
         public Task<SearchItem> SearchAirtistItemsAsync(string q, int limit = 1, int offset = 0, string market = "")
         {
-            return _spotify.SearchItemsAsync(q, SearchType.Artist);
+            return s_spotify.SearchItemsAsync(q, SearchType.Artist);
         }
 
         public Task<FullArtist> GetArtistAsync(string id)
         {
-            return _spotify.GetArtistAsync(id);
+            return s_spotify.GetArtistAsync(id);
         }
 
         public Task<SeveralArtists> GetSeveralArtists(List<string> ids)
         {
-            return _spotify.GetSeveralArtistsAsync(ids);
+            return s_spotify.GetSeveralArtistsAsync(ids);
         }
     }
 }
