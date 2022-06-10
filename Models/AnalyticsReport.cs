f1e4f386bd2fb3c90225f5ecf6102d2ec4e45e8c
ikuo saito <ikuosaito1989@gmail.com>
2022-06-10 23:48:31 +0900

    .NET Core 5.0から6のバージョンアップ #7

--- Models/AnalyticsReport.cs
+++ Models/AnalyticsReport.cs
@@ -1,4 +1,8 @@
-﻿namespace webApi.Models
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace webApi.Models
 {
     public class AnalyticsReport
     {
